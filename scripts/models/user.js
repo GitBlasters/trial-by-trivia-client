906dc2396fb51da9837734705aca11c00285d306
Brandon Haynes <brandonhaynes100@gmail.com>
2018-07-30 16:58:45 -0700

    implementing User.create in user.js

--- scripts/models/user.js
+++ scripts/models/user.js
@@ -12,6 +12,7 @@ var app = app || {};
     Object.keys(rawUserObj).forEach(key => this[key] = rawUserObj[key]);
   }
 
+  // prototype for making username & score html content to render in scoreboard
   User.prototype.toHtml = function(){
     return app.render('user-score-template',this);
   }
@@ -20,25 +21,10 @@ var app = app || {};
 
   User.loadAll = rows => User.all = rows.sort((a,b) => a.score - b.score).map(user => new User(user));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // after accepting and creating a new user, navigate to the quiz page to begin
+  User.create = user =>
+    $.post(`${app.ENVIRONMENT.apiUrl}/api/v1/user`, user)
+      .then(() => page('/quiz'))
+      .catch(errorCallback);
 
 })(app);
