7fcb494d7b5b133519daa9ad2f5f07fd0e3c2b4f
Brandon Haynes <brandonhaynes100@gmail.com>
2018-08-01 16:25:51 -0700

    implementing changes to index.html's scoreboard-template, as well as updating user.js, routes.js, and scoreboard-view.js to display contents from database

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -4,9 +4,10 @@ page('/'
   , ctx => app.homeView.initHomePage(ctx)
 );
 page('/quiz/'
-  , ctx => app.Retriever.setGameOptions(ctx));
+  , ctx => app.Retriever.setGameOptions(ctx)
+);
 page('/quiz/scoreboard'
-  , ctx => app.User.fetchAll(app.scoreboardView.initScoreboardPage(ctx))
+  , ctx => app.User.fetchAll(() => app.scoreboardView.initScoreboardPage(ctx))
 );
 page('/about'
   , ctx => app.aboutView.initAboutPage(ctx)
