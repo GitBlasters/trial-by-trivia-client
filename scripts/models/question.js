7ff37297e4007f147c0e71cf1e8dbaf47a2ebbba
Brandon Haynes <brandonhaynes100@gmail.com>
2018-08-02 16:58:54 -0700

    implementing user name and score input to databse at end of quiz by logic in quiz-view.js

--- scripts/models/question.js
+++ scripts/models/question.js
@@ -4,7 +4,6 @@ var app = app || {};
 
 (function(module){
   const Retriever = {};
-
   // Tom: this function generates a customized api URL based on the options selected by the user. It is called in home-view.js
   Retriever.setGameOptions = () => {
     let gameDifficulty = $('#difficulty-of-questions option:selected').val();
@@ -20,7 +19,7 @@ var app = app || {};
       url = `https://opentdb.com/api.php?amount=10&category=${gameCategory}&difficulty=${gameDifficulty}&type=multiple`;
     }
     $.get(url)
-      .then((data) => { app.Quiz.loadAll(data.results, app.quizView.initQuizView) })
+      .then((data) => { app.Quiz.loadAll(data.results, app.quizView.initQuizView)})
   }
 
   module.Retriever = Retriever;
