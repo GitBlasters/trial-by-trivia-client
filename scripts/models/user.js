6c4cd0412087ac1775bd9909474aadda3d917e08
Brandon Haynes <brandonhaynes100@gmail.com>
2018-08-02 14:33:27 -0700

    implementing changes to user.js for User.create functionality

--- scripts/models/user.js
+++ scripts/models/user.js
@@ -33,7 +33,6 @@ var app = app || {};
   // after accepting and creating a new user, navigate to the quiz page to begin
   User.create = user =>
     $.post(`${app.ENVIRONMENT.apiUrl}/api/v1/user_data`, user)
-      .then(() => page('/quiz'))
       .fail(errorCallback);
   module.User = User;
 })(app);
