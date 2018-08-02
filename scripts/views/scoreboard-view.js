c88cbfbed7e6a6b0446a6fe989728bc25cbe2b16
Brandon Haynes <brandonhaynes100@gmail.com>
2018-08-01 17:22:33 -0700

    updating variable names in scoreboard-view.js

--- scripts/views/scoreboard-view.js
+++ scripts/views/scoreboard-view.js
@@ -5,24 +5,13 @@ var app = app || {};
 (function(module) {
 
   const scoreboardView = {};
-  scoreboardView.dummyData = app.User.all;
-  // [
-  //   {username: 'Gregor', score:100},
-  //   {username: 'Bill', score:200},
-  //   {username:'Joey', score:300},
-  // ];
+
   scoreboardView.initScoreboardPage = () => {
-    scoreboardView.dummyData = app.User.all;
     app.showOnly('.scoreboard-view');
     $('#scores').empty();
     for( let i=0; i <= scoreboardView.dummyData.length; i++) {
-      $('#scores').append(app.render('scoreboard-template', app.scoreboardView.dummyData[i]));
+      $('#scores').append(app.render('scoreboard-template', app.User.all[i]));
     }
-  
-    // $.get(url)
-    //   .then((data) => { app.Quiz.loadAll(data.results, app.quizView.initQuizView) })
-    //   .then(console.log(app.Quiz.all));
-
   }
   module.scoreboardView = scoreboardView;
 })(app)
