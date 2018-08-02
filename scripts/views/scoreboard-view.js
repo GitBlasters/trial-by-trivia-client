c5094e088ff58745d7488abbc8a960592b6194bf
Andrew Helmer <ahelmer7@live.com>
2018-08-02 09:55:17 -0700

    minor change to index.html scoreboard-view.js and module.css

--- scripts/views/scoreboard-view.js
+++ scripts/views/scoreboard-view.js
@@ -9,7 +9,7 @@ var app = app || {};
   scoreboardView.initScoreboardPage = () => {
     app.showOnly('.scoreboard-view');
     $('#scores').empty();
-    for( let i=0; i <= scoreboardView.dummyData.length; i++) {
+    for( let i=0; i <= app.User.all.length; i++) {
       $('#scores').append(app.render('scoreboard-template', app.User.all[i]));
     }
   }
